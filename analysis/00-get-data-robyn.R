56d5c8d204ab5f521d0e8c048640bd9747cf53b0
robynforrest <robynforrest@gmail.com>
2024-09-04 17:08:04 -0700

    updating script for Robyn

--- analysis/00-get-data-robyn.R
+++ analysis/00-get-data-robyn.R
@@ -9,13 +9,13 @@ major_areas <- c("01", "03", "04", "05", "06", "07", "08", "09", "11",
                  "71","72","73","74","75","76","77","99")
 
 
-# dd <- get_all_survey_sets(species_list[1:15],
-#                           ssid = NULL,
-#                           major = major_areas,
-#                           remove_duplicates = TRUE,
-#                           usability = NULL)
-#
-# saveRDS(dd, "data-raw/survey-sets-all-1.rds")
+dd <- get_all_survey_sets(species_list[1:15],
+                          ssid = NULL,
+                          major = major_areas,
+                          remove_duplicates = TRUE,
+                          usability = NULL)
+
+saveRDS(dd, "data-raw/survey-sets-all-1.rds")
 
 dd2 <- get_all_survey_sets(species_list[16:length(species_list)],
                            ssid = NULL,
@@ -24,3 +24,12 @@ dd2 <- get_all_survey_sets(species_list[16:length(species_list)],
                            usability = NULL)
 
 saveRDS(dd2, "data-raw/survey-sets-all-2.rds")
+
+ds <- get_all_survey_samples(species_list,
+                             include_event_info = TRUE,
+                             unsorted_only = FALSE,
+                             random_only = FALSE,
+                             grouping_only = FALSE,
+                             remove_bad_data = TRUE,
+                             remove_duplicates = TRUE)
+saveRDS(ds, "data-raw/survey-samples-all.rds")
