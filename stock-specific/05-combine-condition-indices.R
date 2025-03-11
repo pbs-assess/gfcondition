6cd961ea3cabc07fc543a0eb2c6ede88a7ff1429
ecophilina <paenglis@lakeheadu.ca>
2025-03-10 20:33:16 -0700

    index figure with splits by survey area

--- stock-specific/05-combine-condition-indices.R
+++ stock-specific/05-combine-condition-indices.R
@@ -1,7 +1,14 @@
 # plots combining density indices for all species
 library(tidyverse)
 library(sdmTMB)
+library(patchwork)
 devtools::load_all(".")
+
+# # works for lingcod
+# set_legend_position <- c(0.65,0.18)
+
+set_legend_position <- c(0.4,0.9)
+
 
 source("stock-specific/00-set-options.R")
 spp <- gsub(" ", "-", gsub("\\/", "-", tolower(species)))
@@ -12,6 +19,7 @@ theme_set(ggsidekick:::theme_sleek()+
 model_names <- list.files(paste0("stock-specific/", spp, "/output/cond-pred"),
                           pattern = "", full.names = FALSE)
 
+
 source("analysis/00-species-list.R")
 
 model_name1 <- model_names[1]
@@ -21,7 +29,9 @@ f1 <- list.files(paste0("stock-specific/", spp, "/output/cond-index/",
                         model_name1), pattern = ".rds", full.names = TRUE)
 
 d1 <- purrr::map_dfr(f1, readRDS) |>
-  mutate(group = factor(group, levels = c("immatures", "mature males", "mature females"),
+  mutate(species = ifelse(species == "Rougheye/Blackspotted Rockfish Complex",
+                          "Rougheye/Blackspotted", species),
+         group = factor(group, levels = c("immatures", "mature males", "mature females"),
                         labels = c("Immatures", "Mature males", "Mature females")))
 
 d1$model <- "Density-agnostic"
@@ -39,38 +49,54 @@ d2 <- purrr::map_dfr(f2, readRDS) |>
 d2$model <- "Excluding density-dependence"
 
 
-min_est <- min(d1$est, d2$est)
-max_est <- quantile(c(d1$est, d2$est), 0.9999)
-d1 <- d1 |> mutate(upr_trimmed = ifelse(upr > max_est, max_est, upr))
-d2 <- d2 |> mutate(upr_trimmed = ifelse(upr > max_est, max_est, upr))
+
+
+min_est <- min(d1$est, d2$est,
+               d1$lwr, d2$lwr)
+max_est <- max(d1$est, d2$est,
+               d1$upr, d2$upr
+)
+
+
+# min_est <- min(d1$est, d2$est)
+# max_est <- quantile(c(d1$est, d2$est), 0.9999)
+# d1 <- d1 |> mutate(upr_trimmed = ifelse(upr > max_est, max_est, upr))
+# d2 <- d2 |> mutate(upr_trimmed = ifelse(upr > max_est, max_est, upr))
 
 
 set_ncol <- 1
-set_legend_position <- c(0.65,0.18)
 fig_height <- 4
 fig_width <- 6
 
-d1 |> bind_rows(d2)  |>
+g1 <- d1 |> bind_rows(d2)  |>
   ggplot( aes(year, est, fill = group)) +
   geom_hline(yintercept = 1,
              # linetype = "dotted",
              colour = "grey") +
   geom_line(aes(colour = group, linetype = model)) +
   geom_ribbon(data = d1,
-              aes(ymin = lwr, ymax = upr_trimmed, alpha = group)) +
+              aes(ymin = lwr, ymax = upr, alpha = group)) +
   # scale_y_log10() +
-  facet_wrap(~species,
-             scales = "free_y",
-             ncol = set_ncol) +
+  # facet_wrap(~species,
+  #            scales = "free_y",
+  #            ncol = set_ncol) +
   # coord_cartesian(expand = FALSE, ylim = c(min_est, max_est)) +
   scale_linetype_manual(
     values=c(1,2)
   ) +
-  scale_alpha_discrete(range = c(0.1, 0.3)) +
-  scale_color_viridis_d(option = "D", direction =1) +
-  scale_fill_viridis_d(option = "D", direction =1) +
-  theme(legend.position = set_legend_position,
-        legend.direction = "vertical", legend.box = "horizontal") +
+  scale_alpha_discrete(range = c(0.1, 0.3)) + #, guide = FALSE) +
+  scale_color_viridis_d(option = "D", direction =1) + #, guide = FALSE) +
+  scale_fill_viridis_d(option = "D", direction =1) + #, guide = FALSE) +
+  coord_cartesian(ylim = c(min_est-0.005, max_est+0.005), expand = FALSE) +
+  theme(
+    axis.title.x = element_blank(),
+    legend.title = element_blank(),
+    # legend.position = "none",
+    legend.position = "top",
+    # legend.position = "inside",
+    legend.position.inside = set_legend_position,
+    legend.direction = "vertical", legend.box = "horizontal"
+    ) +
   labs(
     x = "",
     y = "Condition index",
@@ -80,10 +106,145 @@ d1 |> bind_rows(d2)  |>
     linetype = "")
 
 ggsave(paste0("stock-specific/", spp, "/figs/",
-              "all-condition-indices-",
+              spp, "-all-condition-indices-",
               model_name2,
               ".png"),
        height = fig_height,
        width = fig_width
 )
 
+(gl <- d1 |> bind_rows(d2)  |>
+  ggplot( aes(year, log_est, fill = group)) +
+  geom_hline(yintercept = 0,
+             # linetype = "dotted",
+             colour = "grey") +
+  geom_line(aes(colour = group, linetype = model)) +
+  # geom_ribbon(data = d1,
+  #             aes(ymin = lwr, ymax = upr, alpha = group)) +
+  geom_ribbon(data = d1,
+              aes(ymin = log(lwr), ymax = log(upr), alpha = group)) +
+  # scale_y_log10() +
+  # facet_wrap(~species,
+  #            scales = "free_y",
+  #            ncol = set_ncol) +
+  # coord_cartesian(expand = FALSE, ylim = c(min_est, max_est)) +
+  scale_linetype_manual(
+    values=c(1,2)
+  ) +
+  scale_alpha_discrete(range = c(0.1, 0.3)) + #, guide = FALSE) +
+  scale_color_viridis_d(option = "D", direction =1) + #, guide = FALSE) +
+  scale_fill_viridis_d(option = "D", direction =1) + #, guide = FALSE) +
+  # coord_cartesian(ylim = c(min_est-0.005, max_est+0.005), expand = FALSE) +
+  theme(
+    axis.title.x = element_blank(),
+    legend.title = element_blank(),
+    # legend.position = "none",
+    legend.position = "top",
+    # legend.position = "inside",
+    legend.position.inside = set_legend_position,
+    legend.direction = "vertical", legend.box = "horizontal"
+  ) +
+  labs(
+    x = "",
+    y = "Condition index (log scale)",
+    alpha = "",
+    fill = "",
+    colour = "",
+    linetype = ""))
+
+ggsave(paste0("stock-specific/", spp, "/figs/",
+              spp, "-log-condition-indices-",
+              model_name2,
+              ".png"),
+       height = fig_height,
+       width = fig_width
+)
+
+
+f3 <- list.files(paste0("stock-specific/", spp, "/output/cond-index-by-survey/",
+                        model_name1), pattern = ".rds", full.names = TRUE)
+
+d3 <- purrr::map_dfr(f3, readRDS) |>
+  mutate(species = ifelse(species == "Rougheye/Blackspotted Rockfish Complex",
+                          "Rougheye/Blackspotted", species),
+         region = factor(region, levels = c("SYN WCHG", "SYN HS", "SYN QCS", "SYN WCVI")),
+         group = factor(group, levels = c("immatures", "mature males", "mature females"),
+                        labels = c("Immatures", "Mature males", "Mature females")))
+
+d3$model <- "Density-agnostic"
+
+
+f4 <- list.files(paste0("stock-specific/", spp, "/output/cond-index-by-survey/",
+                        model_name2), pattern = ".rds", full.names = TRUE)
+
+d4 <- purrr::map_dfr(f4, readRDS) |>
+  mutate(species = ifelse(species == "Rougheye/Blackspotted Rockfish Complex",
+                          "Rougheye/Blackspotted", species),
+         region = factor(region, levels = c("SYN WCHG", "SYN HS", "SYN QCS", "SYN WCVI")),
+         group = factor(group, levels = c("immatures", "mature males", "mature females"),
+                        labels = c("Immatures", "Mature males", "Mature females")))
+
+d4$model <- "Excluding density-dependence"
+
+
+### probably only necessary for multispecies plots?
+# min_est <- min(d1$est, d2$est, d3$est, d4$est)
+# max_est <- max(d1$est, d2$est, d3$est, d4$est)
+# # max_est <- quantile(c(d1$est, d2$est, d3$est, d4$est), 0.9999)
+min_est <- quantile(c(d1$lwr, d2$lwr, d3$lwr, d4$lwr), 0.025)
+max_est <- quantile(c(d1$upr, d2$upr, d3$upr, d4$upr), 0.975)
+
+# d3 <- d3 |> mutate(upr_trimmed = ifelse(upr > max_est, max_est, upr))
+# d4 <- d4 |> mutate(upr_trimmed = ifelse(upr > max_est, max_est, upr))
+
+g2 <- d3 |> bind_rows(d4)  |>
+  ggplot( aes(year, est, fill = group)) +
+  geom_hline(yintercept = 1,
+             # linetype = "dotted",
+             colour = "grey") +
+  geom_line(aes(colour = group, linetype = model)) +
+  geom_ribbon(data = d3,
+              aes(ymin = lwr, ymax = upr, alpha = group)) +
+  # scale_y_log10() +
+  facet_grid(region~group) +
+  # coord_cartesian(expand = FALSE, ylim = c(min_est, max_est)) +
+  scale_linetype_manual(
+    values=c(1,2)
+  ) +
+  scale_alpha_discrete(range = c(0.1, 0.3)) +
+  scale_color_viridis_d(option = "D", direction =1) +
+  scale_fill_viridis_d(option = "D", direction =1) +
+  coord_cartesian(ylim = c(min_est, max_est), expand = FALSE) +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "none",
+    # legend.position = "bottom",
+    legend.direction = "vertical", legend.box = "horizontal"
+  ) +
+  labs(
+    x = "",
+    y = "Condition index",
+    alpha = "",
+    fill = "",
+    colour = "",
+    linetype = "")
+
+ggsave(paste0("stock-specific/", spp, "/figs/",
+              spp, "-all-condition-indices-split-",
+              model_name2,
+              ".png"),
+       height = fig_height*1.5,
+       width = fig_width
+)
+
+
+(g1 + ggtitle("A. Coastwide")) / (g2+ggtitle("B. Split by survey area")) + plot_layout(heights = c(0.6,1))
+
+
+ggsave(paste0("stock-specific/", spp, "/figs/",
+              spp, "-all-condition-indices-w-split-",
+              model_name2,
+              ".png"),
+       height = fig_height*2,
+       width = fig_width
+)
